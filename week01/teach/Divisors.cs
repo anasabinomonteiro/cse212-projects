ab119e1d426b10719d288550edc0b95293308327
anasabinomonteiro <mon23034@byui.edu>
2025-10-31 19:09:46 -0300

    WEEK01_ACTIVITY done

--- week01/teach/Divisors.cs
+++ week01/teach/Divisors.cs
@@ -2,7 +2,8 @@ public static class Divisors {
     /// <summary>
     /// Entry point for the Divisors class
     /// </summary>
-    public static void Run() {
+    public static void Run()
+    {
         List<int> list = FindDivisors(80);
         Console.WriteLine("<List>{" + string.Join(", ", list) + "}"); // <List>{1, 2, 4, 5, 8, 10, 16, 20, 40}
         List<int> list1 = FindDivisors(79);
@@ -13,12 +14,21 @@ public static class Divisors {
     /// Create a list of all divisors for a number including 1
     /// and excluding the number itself. Modulo will be used
     /// to test divisibility.
+    /// 
     /// </summary>
     /// <param name="number">The number to find the divisor</param>
     /// <returns>List of divisors</returns>
     private static List<int> FindDivisors(int number) {
         List<int> results = new();
         // TODO problem 1
+        for (int i = 1; i < number; i++)
+        {
+            if (number % i == 0) // verify if i is a divisor of number
+            {
+                results.Add(i); // if it's a divisor, add it to the results list
+            }
+        }
+
         return results;
     }
 }
