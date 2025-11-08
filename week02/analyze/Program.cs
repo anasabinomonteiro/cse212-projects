294aeaf2095f1e608979ed4f1c593ab05bf73aef
anasabinomonteiro <mon23034@byui.edu>
2025-11-08 12:50:12 -0300

    Add comments(for me) to program.cs in week02_Analyze

--- week02/analyze/Program.cs
+++ week02/analyze/Program.cs
@@ -4,10 +4,10 @@ Console.WriteLine(MysteryStack1.Run("stressed"));
 Console.WriteLine(MysteryStack1.Run("a nut for a jar of tuna"));
 
 Console.WriteLine("\n======================\nMysteryStack2\n======================");
-Console.WriteLine(MysteryStack2.Run("5 3 7 + *"));
-Console.WriteLine(MysteryStack2.Run("6 2 + 5 3 - /"));
+Console.WriteLine(MysteryStack2.Run("5 3 7 + *")); //50
+Console.WriteLine(MysteryStack2.Run("6 2 + 5 3 - /")); //4
 try {
-    MysteryStack2.Run("3 +");
+    MysteryStack2.Run("3 +"); // does not have enough operands [3]
     Console.WriteLine("WRONG: expected ApplicationException: Invalid Case 1!");
 }
 catch (ApplicationException e) {
@@ -15,7 +15,7 @@ catch (ApplicationException e) {
 }
 
 try {
-    MysteryStack2.Run("5 0 /");
+    MysteryStack2.Run("5 0 /"); // division by zero [5,0] 5->op1 , 0->op2
     Console.WriteLine("WRONG: expected ApplicationException: Invalid Case 2!");
 }
 catch (ApplicationException e) {
@@ -23,7 +23,7 @@ catch (ApplicationException e) {
 }
 
 try {
-    MysteryStack2.Run("3 8 %");
+    MysteryStack2.Run("3 8 %"); // % ->token not recognized [3,8]
     Console.WriteLine("WRONG: expected ApplicationException: Invalid Case 3!");
 }
 catch (ApplicationException e) {
@@ -31,7 +31,7 @@ catch (ApplicationException e) {
 }
 
 try {
-    MysteryStack2.Run("5 3 4 +");
+    MysteryStack2.Run("5 3 4 +"); // too many operands [5,7] condition 2 != 1
     Console.WriteLine("WRONG: expected ApplicationException: Invalid Case 4!");
 }
 catch (ApplicationException e) {
