410f3ebcaee89924726fafb5e9b4fe6b9a32d148
anasabinomonteiro <mon23034@byui.edu>
2025-11-12 21:05:12 -0300

    Week 03 - team assingments Done

--- week03/teach/Basketball.cs
+++ week03/teach/Basketball.cs
@@ -23,14 +23,33 @@ public class Basketball
         reader.TextFieldType = FieldType.Delimited;
         reader.SetDelimiters(",");
         reader.ReadFields(); // ignore header row
-        while (!reader.EndOfData) {
+        while (!reader.EndOfData)
+        {
             var fields = reader.ReadFields()!;
             var playerId = fields[0];
             var points = int.Parse(fields[8]);
+
+            if (players.ContainsKey(playerId))
+            {
+                players[playerId] += points;
+            }
+            else
+            {
+                players[playerId] = points;
+            }
         }
 
-        Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
+        // Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
 
-        var topPlayers = new string[10];
+        // var topPlayers = new string[10];
+        var topPlayers = players
+            .OrderByDescending(x => x.Value)
+            .Take(10)
+            .ToArray();
+
+        foreach (var x in topPlayers)
+        {
+            Console.WriteLine($"Player ID: {x.Key}\t{x.Value} points");
+        }
     }
 }
